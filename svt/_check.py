018ba85790580e0ec6aae81eed9e567d4e3d8ddf
Ali-7800 <alibaz7800@gmail.com>
2025-03-24 20:52:11 -0500

    Check class for common checks:

---
+++ svt/_check.py
@@ -0,0 +1,61 @@
+class Check:
+    """ 
+    This class is a collection of static methods used for checking 
+    that input satisfy conditions
+    """
+    def __init__(self) -> None:
+        pass
+
+    @staticmethod
+    def validity(variable,variable_name:str,valid_variables:list,error_msg=None):
+        """ 
+        This method is for checking a variable is a member of list of
+        valid variables
+        """
+        if variable not in valid_variables:
+            if error_msg is None:
+                raise ValueError("{0} is not a valid {1}, must be one of {2}".format(variable,variable_name,valid_variables))
+            else:
+                raise ValueError(error_msg)
+    
+    @staticmethod
+    def length(list,list_name:str,min_length:int,error_msg=None):
+        """ 
+        This method is for checking a list is of a certian length
+        """
+        if len(list)<min_length:
+            if error_msg is None:
+                raise ValueError("{0} must at least have {1} element/s".format(list_name,min_length))
+            else:
+                raise ValueError(error_msg)
+            
+    @staticmethod
+    def object_class(variable,desired_class,variable_name:str,error_msg=None):
+        """ 
+        This method is for checking a object is an instance of a certian class
+        """
+        if not isinstance(variable,desired_class):
+            if error_msg is None:
+                raise ValueError("{0} must be an instance of {1}".format(variable_name,desired_class.__name__))
+            else:
+                raise ValueError(error_msg)
+    
+    @staticmethod
+    def list_class(list,desired_class,variable_name:str,list_name:str,error_msg=None):
+        """ 
+        This method is for checking every object in a list is an instance of a certian class
+        """
+        for element in list:
+            if not isinstance(element,desired_class):
+                if error_msg is None:
+                    raise ValueError("Every {0} in {1} must be an instance of {2}".format(variable_name,list_name,desired_class.__name__))
+                else:
+                    raise ValueError(error_msg)
+    
+    @staticmethod
+    def condition(condition,error_class,error_msg):
+        """ 
+        This method is for checking a condition is satisfied
+        """
+        if not condition:
+            raise error_class(error_msg)
