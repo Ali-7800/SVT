1df317e5e63e991ef4d0bcc9f463298031203736
Ali-7800 <alibaz7800@gmail.com>
2025-03-28 03:51:27 -0500

    added prefix argument to all static methods

--- svt/SI_units.py
+++ svt/SI_units.py
@@ -10,20 +10,20 @@ class SI:
         pass
 
     @staticmethod
-    def meter():
-        return Unit(symbol = Unit.Symbol(["m"]),dimension = Unit.Dimension(["length"]),prefix= Unit.Prefix(""))
+    def meter(prefix=""):
+        return Unit(symbol = Unit.Symbol(["m"]),dimension = Unit.Dimension(["length"]),prefix= Unit.Prefix(prefix))
     
     @staticmethod
-    def second():
-        return Unit(symbol = Unit.Symbol(["s"]),dimension = Unit.Dimension(["time"]),prefix= Unit.Prefix(""))
+    def second(prefix=""):
+        return Unit(symbol = Unit.Symbol(["s"]),dimension = Unit.Dimension(["time"]),prefix= Unit.Prefix(prefix))
     
     @staticmethod
-    def hertz():
-        return Unit(symbol = Unit.Symbol([],["s"]),dimension = Unit.Dimension([],["time"]),prefix= Unit.Prefix(""),alternate_symbol= Unit.Symbol(["Hz"]))
+    def hertz(prefix=""):
+        return Unit(symbol = Unit.Symbol([],["s"]),dimension = Unit.Dimension([],["time"]),prefix= 1/Unit.Prefix("prefix"),alternate_symbol= Unit.Symbol(["Hz"]))
     
     @staticmethod
-    def kilogram():
-        return Unit(symbol = Unit.Symbol(["g"]),dimension = Unit.Dimension(["mass"]),prefix= Unit.Prefix("k"))
+    def kilogram(prefix="k"):
+        return Unit(symbol = Unit.Symbol(["g"]),dimension = Unit.Dimension(["mass"]),prefix= Unit.Prefix(prefix))
     
     @staticmethod
     def celsius():
@@ -34,8 +34,8 @@ class SI:
         return Unit(symbol = Unit.Symbol(["K"]),dimension = Unit.Dimension(["temperature"]),prefix= Unit.Prefix(""))
     
     @staticmethod
-    def ampere():
-        return Unit(symbol = Unit.Symbol(["A"]),dimension = Unit.Dimension(["current"]),prefix= Unit.Prefix(""))
+    def ampere(prefix=""):
+        return Unit(symbol = Unit.Symbol(["A"]),dimension = Unit.Dimension(["current"]),prefix= Unit.Prefix(prefix))
     
     @staticmethod
     def mole():
@@ -46,50 +46,49 @@ class SI:
         return Unit(symbol = Unit.Symbol(["cd"]),dimension = Unit.Dimension(["luminousity"]),prefix= Unit.Prefix(""))
     
     @staticmethod
-    def coloumb():
-        return Unit(symbol = Unit.Symbol(["A","s"]),dimension = Unit.Dimension(["current","time"]),prefix= Unit.Prefix(""),alternate_symbol= Unit.Symbol(["C"]))
+    def coloumb(prefix=""):
+        return Unit(symbol = Unit.Symbol(["A","s"]),dimension = Unit.Dimension(["current","time"]),prefix= Unit.Prefix(prefix),alternate_symbol= Unit.Symbol(["C"]))
     
     @staticmethod
-    def volt():
+    def volt(prefix=""):
         return Unit(symbol = Unit.Symbol(["g","m","m"],["s","s","s","A"]),
                     dimension = Unit.Dimension(["mass","length","length"],3*["time"]+["current"]),
-                    prefix= Unit.Prefix("k"),
+                    prefix= Unit.Prefix(prefix)*Unit.Prefix("k"),
                     alternate_symbol= Unit.Symbol(["V"]),
-                    alternate_prefix = Unit.Prefix(""))
+                    alternate_prefix = Unit.Prefix(prefix))
     
     @staticmethod
-    def newton():
+    def newton(prefix=""):
         return Unit(symbol = Unit.Symbol(["m","g"],2*["s"]),
                     dimension = Unit.Dimension(["length","mass"],2*["time"]),
-                    prefix= Unit.Prefix("k"),
+                    prefix= Unit.Prefix(prefix)*Unit.Prefix("k"),
                     alternate_symbol = Unit.Symbol(["N"]),
-                    alternate_prefix = Unit.Prefix(""),
+                    alternate_prefix = Unit.Prefix(prefix),
                     )
     
     @staticmethod
-    def joule():
+    def joule(prefix=""):
         return Unit(symbol = Unit.Symbol(["m","m","g"],2*["s"]),
                     dimension = Unit.Dimension(["length","length","mass"],2*["time"]),
-                    prefix= Unit.Prefix("k"),
+                    prefix= Unit.Prefix(prefix)*Unit.Prefix("k"),
                     alternate_symbol = Unit.Symbol(["J"]),
-                    alternate_prefix = Unit.Prefix(""),
+                    alternate_prefix = Unit.Prefix(prefix),
                     )
     
     @staticmethod
-    def watt():
+    def watt(prefix=""):
         return Unit(symbol = Unit.Symbol(["m","m","g"],3*["s"]),
                     dimension = Unit.Dimension(["length","length","mass"],3*["time"]),
-                    prefix= Unit.Prefix("k"),
+                    prefix= Unit.Prefix(prefix)*Unit.Prefix("k"),
                     alternate_symbol = Unit.Symbol(["W"]),
-                    alternate_prefix = Unit.Prefix(""),
+                    alternate_prefix = Unit.Prefix(prefix),
                     )
     
     @staticmethod
-    def pascal():
+    def pascal(prefix=""):
         return Unit(symbol = Unit.Symbol(["m","g"],2*["s","m"]),
                     dimension = Unit.Dimension(["length","mass"],2*["time","length"]),
-                    prefix= Unit.Prefix("k"),
+                    prefix= Unit.Prefix(prefix)*Unit.Prefix("k"),
                     alternate_symbol = Unit.Symbol(["Pa"]),
-                    alternate_prefix = Unit.Prefix(""),
+                    alternate_prefix = Unit.Prefix(prefix)*Unit.Prefix(""),
                     )
-
